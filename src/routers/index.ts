821e6598f7464ece6cc500a096309ac5f3e4d75f
thecao <tcao@star.global>
2022-05-16 21:53:23 +0700

    chore: update tests

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,4 +1,3 @@
 export * from './types.d';
 export { default as ScreenNames } from './Routes';
-export { default as RootNavigator } from './rootNavigator';
 export { default as Navigator } from './navigator';
--- src/routers/navigator.tsx
+++ src/routers/navigator.tsx
@@ -1,11 +1,57 @@
-import RootNavigator from './rootNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { RootStackParamList } from './types';
+import ScreenNames from './Routes';
+import { AuthenticationScreen, TodoScreen } from '@screens';
+import { useAppState } from '@hooks';
+import { selectAuthenticated } from '@selectors';
+import { resetAuthentication } from '../reducers';
 
 const Navigator = () => {
   return (
     <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
+  );
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const RootNavigator = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(selectAuthenticated);
+  const appState = useAppState();
+
+  /*
+    Reset user authenticated state when user go to background for security
+   */
+  useEffect(() => {
+    if (appState !== 'active') {
+      dispatch(resetAuthentication());
+    }
+  }, [appState, dispatch]);
+
+  return (
+    <Stack.Navigator>
+      {isAuthenticated ? (
+        <Stack.Screen
+          name={ScreenNames.Todo}
+          component={TodoScreen}
+          options={{
+            headerTitle: 'Todo List',
+          }}
+        />
+      ) : (
+        <Stack.Screen
+          name={ScreenNames.Authentication}
+          component={AuthenticationScreen}
+          options={{ headerShown: false }}
+        />
+      )}
+    </Stack.Navigator>
   );
 };
 
